3cc5d39e4ffed2934803c8f9c7f5086a594d4018
WalterAltamirano <altamiranow4@gmail.com>
2026-07-30 20:20:16 -0300

    wip: agrego algunas rutas para la navegacion de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import {Routes, Route} from 'react-router'
 import Home  from './components/Home.jsx'
 import NotFoundContent from './components/reusable/NotFoundContent.jsx'
+import Layout from './utils/Layout.jsx'
+import Propuestas from './components/Propuestas.jsx'
+import Catalogo from './components/Catalogo.jsx'
 function App() {
 
   //Manejo de rutas...
   return (
     <Routes>
-      <Route path="home" element={<Home />} />
-      <Route path="*" element={<NotFoundContent />} />
+      <Route element={<Layout />}>
+        <Route path="home" element={<Home />} />
+        <Route path="nuestrasPropuestas" element={<Propuestas />} />
+        <Route path="catalogo" element={<Catalogo />} />
+        <Route path="*" element={<NotFoundContent />} />
+      </Route>
     </Routes>
   )
 }
