025fb8ef4901e3ea5d4fdd90aa5efb4446a79cb3
WalterAltamirano <altamiranow4@gmail.com>
2026-08-16 19:36:42 -0300

    refac: cambio tag y modifico estilos

--- src/components/reusable/Item.jsx
+++ src/components/reusable/Item.jsx
@@ -3,7 +3,7 @@ import '../../styles/item.css'
 import backup from '../../assets/backup-image-item.jpeg'
 const Item =  ({name}) => {
     return(
-        <aside className="container-item">
+        <div className="container-item">
             <div className='container-item-image'>
                 <img className="item-image" src={backup} alt="An golden hour image with some explosion" />
             </div>
@@ -11,7 +11,7 @@ const Item =  ({name}) => {
                 <h3>{name}</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
             </div>
-        </aside>
+        </div>
     );
 }
 export default Item;
